59fd68a414f3f7d0a773c14fbb5735074a7abfa7
dharmendra <dharmendra@thesolarlabs.com>
2021-07-09 22:48:15 +0530

    added serializer class

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -13,7 +13,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
   class Meta:
     model = models.UserProfile
-    fields = ('id', 'email', 'password')
+    fields = ('id', 'email', 'name', 'password')
     extra_kwargs = {
       'password': {
         'write_only': True,
